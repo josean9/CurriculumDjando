bde4bf1c0a54fa63260f8b757711de82c0f0a70a
jose antonio <jsancesc@gmail.com>
2024-11-14 19:49:52 +0100

    commit

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -77,6 +77,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': 'Jacobo',
+        'PASSWORD': 'contrasena1'
     }
 }
 
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'Es'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
