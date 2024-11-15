4831df12724cecd23d0c5958a975c06dfbd85795
jose antonio <jsancesc@gmail.com>
2024-11-15 19:16:17 +0100

    el html actualizado

--- webpersonal/blog/views.py
+++ webpersonal/blog/views.py
@@ -5,4 +5,4 @@ from .models import Post
 
 def home(request):
     posts = Post.objects.all()
-    return render(request, "blog/home.html")
+    return render(request, "blog/home.html", {'posts': posts})
