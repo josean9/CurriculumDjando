ded258e2fcccf3f89381272028b0b421d43f5fbe
jose antonio <jsancesc@gmail.com>
2024-11-17 18:54:26 +0100

    ns

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -19,10 +19,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
+
 urlpatterns = [
- path('', views.home, name="home"),
- path('about-me/', views.about, name="about"),
- path('portfolio/', views.portfolio, name="portfolio"),
- path('contact/', views.contact, name="contact"),
- path('admin/', admin.site.urls),
+    path('', views.home, name="home"),
+    path('about-me/', views.about, name="about-me"),  # Cambiado el nombre a "about-me" para que coincida
+    path('portfolio/', views.portfolio, name="portfolio"),
+    path('contact/', views.contact, name="contact"),
+    path('admin/', admin.site.urls),
 ]
