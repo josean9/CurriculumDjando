210f5504e4dcddbec798fd765f8debcb151b094c
jose antonio <jsancesc@gmail.com>
2024-11-18 16:29:59 +0100

    models actualizado

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,9 +1,11 @@
 
-# Create your models here.
 from django.db import models
 class Project(models.Model):
- title = models.CharField(max_length=200)
- description = models.TextField()
- image = models.ImageField()
- created = models.DateTimeField(auto_now_add=True)
- updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    class Meta:
+        verbose_name = "proyecto"
+        verbose_name_plural = "proyectos"
--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -29,16 +29,15 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',	
-    'core',	
-    'portfolio',
+    'django.contrib.staticfiles',
+    'core',
+    'portfolio.apps.PortfolioConfig', # <====
 ]
 
 MIDDLEWARE = [
