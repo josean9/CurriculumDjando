f8d47ede812e10b7daffc04d25f8c5f766ad4121
jose antonio <jsancesc@gmail.com>
2024-11-18 15:47:52 +0100

    base.html actualizado

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -122,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+# Configuración de archivos estáticos
+STATIC_URL = '/static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
