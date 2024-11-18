583082ae05d4302d3d8a933698478667cc130089
jose antonio <jsancesc@gmail.com>
2024-11-18 16:39:05 +0100

    urls de web personaly el mdeia

--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -19,12 +19,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from core import views
-
+from django.conf import settings # <=====
 urlpatterns = [
-    path('', views.home, name="home"),
-    path('about-me/', views.about, name="about-me"),
-    path('portfolio/', views.portfolio, name="portfolio"),
-    path('contact/', views.contact, name="contact"),
-    path('admin/', admin.site.urls),
-    
+ path('', views.home, name="home"),
+ path('about-me/', views.about, name="about"),
+ path('portfolio/', views.portfolio, name="portfolio"),
+ path('contact/', views.contact, name="contact"),
+ path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+ from django.conf.urls.static import static
+ urlpatterns += static(settings.MEDIA_URL,
+ document_root=settings.MEDIA_ROOT)
