1ed1edca1f13219ebce6ad866220cc41646c6ba7
jose antonio <jsancesc@gmail.com>
2024-11-19 16:31:07 +0100

    terminado

--- webpersonal/portfolio/views.py
+++ webpersonal/portfolio/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
 
+from django.shortcuts import render
+from .models import Project
 def portfolio(request):
- return render(request, "core/portfolio.html")
+ projects = Project.objects.all()
+ return render(request, "portfolio/portfolio.html",
+ {'projects':projects}) 
